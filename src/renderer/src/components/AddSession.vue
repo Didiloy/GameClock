4c7677903fd0ed3127db19cab7c28f9300871a94
dylan <didiloy2001@gmail.com>
2024-05-05 12:28:31 +0200

    added ability to enter a formula add the time of the session

--- src/renderer/src/components/AddSession.vue
+++ src/renderer/src/components/AddSession.vue
@@ -1,68 +1,118 @@
 <template>
-  <Card class="card"
-        :pt="{
-            root: { style: 'box-shadow: 0px 0px 0px 0px;' },
-            content: { style: 'height:100%; width: auto;' }
-        }">
+  <Card
+    class="card"
+    :pt="{
+      root: { style: 'box-shadow: 0px 0px 0px 0px;' },
+      content: { style: 'height:100%; width: auto;' },
+    }"
+  >
     <template #content>
       <div class="content-container">
         <div class="as-input">
           <AutoComplete
-              class="input-field mb10"
-              v-model="game"
-              placeholder="Nom du jeu"
-              :suggestions="items"
-              @complete="autocomplete"
+            class="input-field mb10"
+            v-model="game"
+            placeholder="Nom du jeu"
+            :suggestions="items"
+            @complete="autocomplete"
           ></AutoComplete>
           <div class="as-duration-input">
-            <InputNumber
-                v-if="!is_chrono_running"
-                class="input-field input-number"
-                v-model="duration"
-                placeholder="Durée en minute"
-            ></InputNumber>
+            <span class="as-icon" @click="toggleOverlay"
+              ><i class="pi pi-info"></i
+            ></span>
+            <OverlayPanel ref="op">
+            <span>
+              Vous pouvez entrer une durée en minutes ou utiliser une formule
+              pour additionner plusieurs durées. Par exemple, pour ajouter 10
+              minutes et 20 minutes, entrez "=10+20".
+            </span>
+            </OverlayPanel>
+            <InputText
+              v-if="!is_chrono_running"
+              class="input-number"
+              v-model="duration"
+              placeholder="Durée en minute"
+            ></InputText>
             <span v-else class="chrono-time">
               {{ convertSecondsToHourMinutesSeconds(duration_seconds) }}
             </span>
             <div>
-              <Button icon="pi pi-clock"
-                      :label="is_chrono_running > 0 ? 'Stop' : ''" @click="startStopWatch"
-                      :style="'background-color:' + getChronoButtonColor() +';'"
-                      class="as-chrono-button"
+              <Button
+                icon="pi pi-clock"
+                :label="is_chrono_running > 0 ? 'Stop' : ''"
+                @click="startStopWatch"
+                :style="'background-color:' + getChronoButtonColor() + ';'"
+                class="as-chrono-button"
               />
             </div>
           </div>
         </div>
         <div class="as-fun-selector mb10">
-          <ToggleButton v-model="toggle_fun" onLabel="Fun" offLabel="Fun"
-                        style="background-color: var(--green-400); color: whitesmoke"/>
-          <ToggleButton v-model="toggle_neutre" onLabel="Neutre" offLabel="Neutre"
-                        style="background-color: var(--gray-400); color: whitesmoke"/>
-          <ToggleButton v-model="toggle_nul" onLabel="Nul" offLabel="Nul"
-                        style="background-color: var(--red-400); color: whitesmoke"/>
+          <ToggleButton
+            v-model="toggle_fun"
+            onLabel="Fun"
+            offLabel="Fun"
+            class="toggle-button"
+            style="color: var(--text-primary)"
+            :pt="{
+              root: {
+                style: toggle_fun
+                  ? 'background-color: var(--primary-400);'
+                  : 'background-color: whitesmoke;',
+              },
+            }"
+          />
+          <ToggleButton
+            v-model="toggle_neutre"
+            onLabel="Neutre"
+            offLabel="Neutre"
+            class="toggle-button"
+            style="color: var(--text-primary)"
+            :pt="{
+              root: {
+                style: toggle_neutre
+                  ? 'background-color: var(--primary-400);'
+                  : 'background-color: whitesmoke;',
+              },
+            }"
+          />
+          <ToggleButton
+            v-model="toggle_nul"
+            onLabel="Nul"
+            offLabel="Nul"
+            class="toggle-button"
+            style="color: var(--text-primary)"
+            :pt="{
+              root: {
+                style: toggle_nul
+                  ? 'background-color: var(--primary-400);'
+                  : 'background-color: whitesmoke;',
+              },
+            }"
+          />
         </div>
         <Button
-            label="Ajouter"
-            :icon="icon"
-            class="btn-add"
-            @click="addNewSession"
-            :loading="loading"
+          label="Ajouter"
+          :icon="icon"
+          class="btn-add"
+          @click="addNewSession"
+          :loading="loading"
         ></Button>
       </div>
     </template>
   </Card>
-  <Toast/>
+  <Toast />
 </template>
 <script setup>
-import {onMounted, ref, watch} from "vue";
-import {useToast} from "primevue/usetoast";
-import {useStore} from "../store/store";
-import {storeToRefs} from "pinia";
-import {addSession} from "../database/database";
-import {getPreferences} from "../preferences/preferences";
+import { onMounted, ref, watch } from "vue";
+import { useToast } from "primevue/usetoast";
+import { useStore } from "../store/store";
+import { storeToRefs } from "pinia";
+import { addSession } from "../database/database";
+import { getPreferences } from "../preferences/preferences";
 
 const store = useStore();
-const {games} = storeToRefs(store);
+const { games } = storeToRefs(store);
 const toast = useToast();
 const props = defineProps(["teamName", "gameName"]);
 const all_games = ref(games);
@@ -73,9 +123,9 @@ const game = ref("");
 const duration = ref();
 const was_cool = ref({});
 const options_cool = ref([
-  {name: "Bien", value: true},
-  {name: "Neutre", value: undefined},
-  {name: "Nul", value: false},
+  { name: "Bien", value: true },
+  { name: "Neutre", value: undefined },
+  { name: "Nul", value: false },
 ]);
 const toggle_fun = ref(false);
 const toggle_neutre = ref(false);
@@ -85,7 +135,7 @@ watch(toggle_fun, () => {
   if (toggle_fun.value) {
     toggle_neutre.value = false;
     toggle_nul.value = false;
-    was_cool.value = {value: true};
+    was_cool.value = { value: true };
   }
 });
 
@@ -93,7 +143,7 @@ watch(toggle_neutre, () => {
   if (toggle_neutre.value) {
     toggle_fun.value = false;
     toggle_nul.value = false;
-    was_cool.value = {value: undefined};
+    was_cool.value = { value: undefined };
   }
 });
 
@@ -101,7 +151,7 @@ watch(toggle_nul, () => {
   if (toggle_nul.value) {
     toggle_neutre.value = false;
     toggle_fun.value = false;
-    was_cool.value = {value: false};
+    was_cool.value = { value: false };
   }
 });
 
@@ -136,6 +186,11 @@ setInterval(() => {
   }
 }, 1000);
 
+const op = ref();
+const toggleOverlay = () => {
+  op.value.toggle(event);
+};
+
 const autocomplete = (event) => {
   let tmpArray = all_games.value.filter((item) => {
     return item.name.toLowerCase().includes(event.query.toLowerCase());
@@ -168,13 +223,45 @@ function convertSecondsToHourMinutesSeconds(seconds) {
   return result;
 }
 
+const regex = new RegExp("^[1-9]\\d*$"); //vérifie si le nombre entré ne commecne pas par un 0
+
 async function addNewSession() {
   loading.value = true;
+
+  if (duration.value[0] === "=") {
+    let operandes = duration.value.slice(1).split("+");
+    duration.value = 0;
+    operandes.forEach((operand) => {
+      if (!regex.test(operand)) {
+        toast.add({
+          severity: "error",
+          summary: "",
+          detail:
+            "Les opérandes d'une formule doivent être des nombres entiers et vous ne pouvez utiliser que l'addition.",
+          life: 3000,
+        });
+        loading.value = false;
+        duration.value = "";
+        return;
+      }
+      duration.value += parseInt(operand);
+    });
+  } else if (!regex.test(duration.value || !game.value)) {
+    toast.add({
+      severity: "error",
+      summary: "",
+      detail: "Veuillez remplir tous les champs correctement.",
+      life: 3000,
+    });
+    loading.value = false;
+    return;
+  }
+
   const success = await addSession(
-      props.teamName,
-      game.value,
-      duration.value,
-      was_cool.value.value
+    props.teamName,
+    game.value,
+    duration.value,
+    was_cool.value.value,
   );
   loading.value = false;
   if (success) {
@@ -199,11 +286,11 @@ async function addNewSession() {
     });
   }
   game.value = "";
-  duration.value = 0;
+  duration.value = "";
 }
 
 function getChronoButtonColor() {
-  return is_chrono_running.value ? 'var(--red-400)' : 'var(--primary-400)';
+  return is_chrono_running.value ? "var(--red-400)" : "var(--primary-400)";
 }
 </script>
 <style scoped>
@@ -239,11 +326,11 @@ function getChronoButtonColor() {
 }
 
 .input-field {
-  width: 200px;
+  width: 300px;
 }
 
 .input-number {
-  margin-right: 108px;
+  margin-right: 80px;
 }
 
 .as-fun-selector {
@@ -253,6 +340,16 @@ function getChronoButtonColor() {
   align-items: center;
   margin-top: 10px;
   width: 100%;
+  background-color: whitesmoke;
+  border-radius: 10px;
+  padding: 5px;
+}
+
+.toggle-button {
+  width: 90%;
+  height: 90%;
+  border: none;
+  margin: 0 5px;
 }
 
 .as-chrono-button {
@@ -263,6 +360,23 @@ function getChronoButtonColor() {
   margin-right: 100px;
   font-size: 1.5rem;
   font-weight: bold;
+}
+
+.as-icon {
+  margin-right: 10px;
+  background-color: whitesmoke;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  color: var(--primary-400);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.as-icon:hover {
+  cursor: pointer;
 }
 
 .mb10 {
