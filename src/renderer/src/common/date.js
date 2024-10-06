7aaf05dd9cea075c2f7ae29ba4f67874f9a691da
Dylan <78911744+Didiloy@users.noreply.github.com>
2024-10-06 16:36:08 +0200

    chanded date of halloween

--- src/renderer/src/common/date.js
+++ src/renderer/src/common/date.js
@@ -1,9 +1,8 @@
 export function isHalloween() {
   const today = new Date();
-  const before_halloween = new Date(today.getFullYear(), 9, 28); // October 28th
+  const before_halloween = new Date(today.getFullYear(), 9, 5); // October 5th
   const after_halloween = new Date(today.getFullYear(), 10, 3); // November 3rd
-  // return today.getTime() > before_halloween.getTime() && today.getTime() < after_halloween.getTime()
-  return true;
+  return today.getTime() > before_halloween.getTime() && today.getTime() < after_halloween.getTime()
 }
 
 export function isChristmas() {
